bd8ef8d89767e4226bc56b6fc976c60ecd050cae
Thoai1503 <vothoai1503@gmail.com>
2025-08-22 20:49:41 +0700

    log user & token

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get("/register", function (req, res, next) {
 });
 router.get("/myprofile", checkToken, function (req, res, next) {
   console.log("Token: " + req.cookies.token);
+  console.log("User: " + JSON.stringify(req.user));
   res.render("myprofile", { title: "myprofile", user: req.user });
 });
 
